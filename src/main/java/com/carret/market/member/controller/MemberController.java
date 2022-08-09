cf009eff28c63d8a8f9e4ae835dc7897aea2c4b5
Shin seongsu <tlstjdtn321@naver.com>
2022-08-09 23:45:08 +0900

    feat: 로그인화면 및 회원가입 만들기

---
+++ src/main/java/com/carret/market/member/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.carret.market.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MemberController {
+
+    @GetMapping("/login")
+    public ModelAndView login(ModelAndView mav) {
+        mav.setViewName("/member/login");
+
+        return mav;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView logout(ModelAndView mav) {
+        mav.setViewName("/member/register");
+        return mav;
+    }
+
+}

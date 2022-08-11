44a5f96fb7eb2193bc4acd580d7b5ee2360cc564
Shin seongsu <tlstjdtn321@naver.com>
2022-08-12 02:14:15 +0900

    feat: 로그인 기능 추가

---
+++ src/main/java/com/carret/market/web/login/LoginController.java
@@ -0,0 +1,21 @@
+package com.carret.market.web.login;
+
+import java.util.Objects;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginView(Model model, @RequestParam(required = false) String error) {
+        if(Objects.nonNull(error)) {
+            model.addAttribute("errorMessage", "로그인 실패하였습니다.");
+        }
+
+        return "/login/login";
+    }
+
+}

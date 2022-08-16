0ec70a7d21be71df30773f3f1b78003f7cbd70e0
Shin seongsu <tlstjdtn321@naver.com>
2022-08-16 12:45:34 +0900

    fix: 코드 컨밴션 지키기

--- src/main/java/com/carret/market/config/jpa/QuerydslConfiguration.java
+++ src/main/java/com/carret/market/config/jpa/QuerydslConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QuerydslConfiguration {
+
     @PersistenceContext
     private EntityManager entityManager;
 
